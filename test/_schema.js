6b8d3e8e9036a37b0d0c0ff79fa46cfb39ea4f1d
Mike Fix <mrfix84@gmail.com>
2017-05-07 21:42:38 -0500

    Refactor test

--- test/_schema.js
+++ test/_schema.js
@@ -1,28 +1,28 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql')
-const { Phone, UnixDate, CreditCard, State } = require('..')
+const gnt = require('..')
 
 module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
       phone: {
-        type: Phone,
+        type: gnt.Phone,
         args: { value: { type: GraphQLString } },
         resolve: (root, args) => args.value
       },
       date: {
-        type: UnixDate,
+        type: gnt.UnixDate,
         args: { value: { type: GraphQLString } },
         resolve: (root, args) => args.value
       },
       card: {
-        type: CreditCard,
+        type: gnt.CreditCard,
         args: { value: { type: GraphQLString } },
         resolve: (root, args) => args.value
       },
       state: {
-        type: State,
-        args: { value: { type: State } },
+        type: gnt.State,
+        args: { value: { type: gnt.State } },
         resolve: (root, args) => args.value
       }
     }
