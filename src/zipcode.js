ee3ce2043c8c1c982c63ae25eea79e5f881ad920
Mike Fix <mrfix84@gmail.com>
2017-08-14 19:03:00 -0700

    Make errors better

--- src/zipcode.js
+++ src/zipcode.js
@@ -3,14 +3,15 @@ const validZips = require('./_zips')
 
 const check = value => {
   if (validZips.has(value)) return value
+  throw new Error(`${value} is not a valid zipcode`)
 }
 
 const leftPad = (v, n, c = '0') =>
   String(v).length >= n ? String(v) : (String(c).repeat(n) + v).slice(-n);
 
 const validate = value => {
-  if (!value || isNaN(value)) return null
-  return check(leftPad(value, 5)) || null
+  if (!value || isNaN(value)) throw new Error(`${value} is not a parsable to a zipcode`)
+  return check(leftPad(value, 5))
 }
 
 module.exports = new GraphQLScalarType({
