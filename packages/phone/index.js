e045cbce7ab2f9b7c09809cd4ff47d15abcdc4a3
Mike Fix <mrfix84@gmail.com>
2017-10-13 20:10:49 -0700

    Add support for toll free numbers
    Closes #7

--- packages/phone/index.js
+++ packages/phone/index.js
@@ -1,16 +1,22 @@
 const { GraphQLScalarType } = require('graphql')
 const phone = require('phone')
 
+const TOLL_FREE_PREFIX = ['800', '844', '855', '866', '877', '888']
+
+const parse = (value) => {
+  const num = String(value).replace(/\D/g, '').slice(-10)
+  if (num.length === 10 && TOLL_FREE_PREFIX.some(v => v === num.substr(0, 3))) {
+    return `+1${num}`
+  }
+  return phone(String(value))[0]
+}
+
 // Index 0 is phone, index 1 is country code
 module.exports = new GraphQLScalarType({
   name: 'PhoneNumber',
-  serialize: (value) => {
-    return phone(value.toString())[0]
-  },
-  parseValue: (value) => {
-    return phone(value.toString())[0]
-  },
+  serialize: parse,
+  parseValue: parse,
   parseLiteral (ast) {
-    return phone(ast.value.toString())[0]
+    return parse(ast.value)
   }
 })
